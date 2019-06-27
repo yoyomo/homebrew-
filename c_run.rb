8d7666463a545b7003d8ab1562f13d8461032bf6
Armando Ortiz <armando@solairo.co.jp>
2019-06-27 16:02:00 +0900

    add tgz

--- c_run.rb
+++ c_run.rb
@@ -1,7 +1,7 @@
 class CRun < Formula
   desc "Compile and Run C code using only one command line"
   homepage "https://github.com/yoyomo/c_run"
-  url "https://raw.githubusercontent.com/yoyomo/c_run/master/c_run-1.0.tar.gz"
+  url "https://raw.githubusercontent.com/yoyomo/c_run/master/c_run-1.0.tgz"
   sha256 "b830718c78f1bf9f9277011fce6cff139bd90e00f32fc9f0005bd010fc1719a7"
 
   def install

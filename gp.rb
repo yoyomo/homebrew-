866167639b17929193fe07d35277a55723549f30
Armando Ortiz <armando@solairo.co.jp>
2019-06-28 14:44:56 +0900

    update gp

--- gp.rb
+++ gp.rb
@@ -1,8 +1,8 @@
 class Gp < Formula
   desc "git add .; git commit -m \"message\"; git push origin head => one liner"
   homepage "https://github.com/yoyomo/gp"
-  url "https://raw.githubusercontent.com/yoyomo/gp/master/gp-1.0.tar.gz"
-  sha256 "e054dadb64528eaf68d0a710fa5bd7f4fc3bc936383ff3883063735dceba9f27"
+  url "https://raw.githubusercontent.com/yoyomo/gp/master/versions/gp-1.0.tar.gz"
+  sha256 "760b6ec304f8deda59629b04c7214d63c8896f382e10bea3d7e478ac2495349d"
 
   def install
     bin.install "gp"

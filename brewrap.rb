5e23bc0de2425d1a15f85725c9a3df1105cf490c
Armando Ortiz <armando@solairo.co.jp>
2019-07-03 15:44:06 +0900

    generate formulas

--- brewrap.rb
+++ brewrap.rb
@@ -1,14 +1,11 @@
 class Brewrap < Formula
-  desc "wrap packages for brew"
+  desc "package a project ready for brew"
   homepage "https://github.com/yoyomo/brewrap"
   url "https://raw.githubusercontent.com/yoyomo/brewrap/master/versions/v1.0.tar.gz"
-  sha256 "8108fbee2b404cde56d0e1f24ceb800b865d93d8b9af6e55d1b01019f7347fc4"
+  sha256 "dc688dcdc3a985940e641982394e52acf85f28b2c5f1ce709f4f397c4454fe07  -"
 
   def install
     bin.install "brewrap"
   end
-
-  test do
-    system "false"
-  end
 end
+

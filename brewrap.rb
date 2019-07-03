01a3cfc0c30d8495f1b6dfcc1e8e6c5d5bb842c7
Armando Ortiz <armando@solairo.co.jp>
2019-07-03 15:50:04 +0900

    update brewrap

--- brewrap.rb
+++ brewrap.rb
@@ -2,7 +2,7 @@ class Brewrap < Formula
   desc "package a project ready for brew"
   homepage "https://github.com/yoyomo/brewrap"
   url "https://raw.githubusercontent.com/yoyomo/brewrap/master/versions/v1.0.tar.gz"
-  sha256 "dc688dcdc3a985940e641982394e52acf85f28b2c5f1ce709f4f397c4454fe07  -"
+  sha256 "dc688dcdc3a985940e641982394e52acf85f28b2c5f1ce709f4f397c4454fe07"
 
   def install
     bin.install "brewrap"

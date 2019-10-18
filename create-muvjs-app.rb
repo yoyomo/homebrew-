5327e0e913f946f823b9e2ea186d44a1d22bd4da
Armando Ortiz <armando@solairo.co.jp>
2019-10-18 16:00:56 +0900

    update create-muvjs-app

--- create-muvjs-app.rb
+++ create-muvjs-app.rb
@@ -1,7 +1,7 @@
 class CreateMuvjsApp < Formula
   desc "brew installed command to create a MuvJS app"
   homepage "https://github.com/yoyomo/create-muvjs-app"
-  url "https://raw.githubusercontent.com/yoyomo/create-muvjs-app/master/versions/v1.1.tar.gz"
+  url "https://raw.githubusercontent.com/yoyomo/create-muvjs-app/master/versions/v2.1.tar.gz"
   sha256 "305dd767ae8d49da39c947d592a94a1cf6b91246a5560de793fcccaafb2241b1"
 
   def install
